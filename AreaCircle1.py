acf19467cb2efdcd04189a53bd03cdb00630a323
KING JOHN ADAMZ R. PAGLINAWAN <90814383+kingpaglinawan@users.noreply.github.com>
2022-10-13 07:56:13 +0800

    Add files via upload

---
+++ AreaCircle1.py
@@ -0,0 +1,15 @@
+rad = float(input("Enter the radius: "))
+PI = 3.14
+area = PI*rad*rad
+diameter = rad*2
+
+print("Area of circle is:", area)
+print("Diameter of a circle:", diameter)
+
+dia = float(input("Enter the diameter: "))
+PI = 3.14
+area2 = PI/4*dia*dia
+radius = dia/2
+
+print("Area of circle is:", area2)
+print("Diameter of a circle:", radius)
---
+++ CPEN65-AreaCircle.py
@@ -0,0 +1,21 @@
+class Circle:
+    def __init__(self,radius,diameter):
+        self.radius = radius
+        self.diameter = diameter
+    def area():
+        pi = 3.14
+        radius = int(input("Enter the radius of the circle: "))
+        print("The diameter of a circle is: ", radius+radius)
+        print("The area of a circle is: ", pi*radius*radius)
+    def area2():
+        pi = 3.14
+        diameter = int(input("Enter the diameter of the circle:"))
+        print("The radius of a circle is: ", diameter/2)
+        print("The area of a circle is: ", pi/4*diameter*diameter)
+
+Circle.area()
+Circle.area2()
+
+
+
+
